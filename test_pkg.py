c809c0570a0c9b585431042d710bcd1782df74bd
rtmigo <art.galkin@gmail.com>
2021-05-31 21:44:53 +0300

    ci + lint

--- test_pkg.py
+++ test_pkg.py
@@ -2,6 +2,21 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('from awscmds import LambdaDockerPipeline')
+        for name in ['docker_build',
+                     'docker_stop',
+                     'docker_push_to_ecr',
+                     'docker_run',
+                     'lambda_function_update',
+                     'lambda_function_wait_updated',
+                     'ecr_delete_images_all',
+                     'ecr_delete_images_by_json',
+                     'print_header',
+                     'set_header_prefix',
+                     'aws_create_credentials_file',
+                     'aws_get_default_credentials_file_path',
+                     'aws_create_credentials_file_on_need',
+                     'LambdaDockerPipeline',
+                     'Stage']:
+            pkg.run_python_code(f'from awscmds import {name}')
 
     print("\nPackage is OK!")
